26a8b625a8fc617061da841e8479d179d8a0b959
Jenzzo Joyosa <125535579+jjjoyosa@users.noreply.github.com>
2024-05-21 20:17:59 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://tic-cat-toe-backend.vercel.app/", {
       autoConnect: true,
     });
 
